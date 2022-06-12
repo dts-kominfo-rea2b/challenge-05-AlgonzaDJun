25e8e1490193e519680a191bdfe62aa528ab8e98
AlgonzaDJun <algonza.20050@mhs.unesa.ac.id>
2022-06-13 00:40:24 +0700

    first

--- index.js
+++ index.js
@@ -6,15 +6,44 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+let arr = [];
+
+const sorter = (nama, callback) => {
+  let result = callback(nama);
+  return result;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (array) => {
+  array.sort((a, b) => {
+    if (a.toLowerCase() < b.toLowerCase()) return -1;
+    if (a.toLowerCase() > b.toLowerCase()) return 1;
+    return 0;
+  });
+
+  for (let [index, val] of array.entries()) {
+    arr.push(index + 1 + ". " + val);
+  }
+
+  return arr;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (array) => {
+  array.sort((a, b) => {
+    if (a.toLowerCase() < b.toLowerCase()) return 1;
+    if (a.toLowerCase() > b.toLowerCase()) return -1;
+    return 0;
+  });
+
+  for (let [index, val] of array.entries()) {
+    arr.push(index + 1 + ". " + val);
+  }
+
+  return arr;
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
