577ef76a1b16d9ec238a7c4ff4320681eaedb23a
AlgonzaDJun <algonza.20050@mhs.unesa.ac.id>
2022-06-13 00:56:20 +0700

    second

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
+
 let arr = [];
 
 const sorter = (nama, callback) => {
   let result = callback(nama);
-  return result;
+
+  for (let [index, val] of result.entries()) {
+    arr.push(index + 1 + ". " + val);
+  }
+  return arr;
 };
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
@@ -19,14 +24,10 @@ const sortAscending = (array) => {
   array.sort((a, b) => {
     if (a.toLowerCase() < b.toLowerCase()) return -1;
     if (a.toLowerCase() > b.toLowerCase()) return 1;
-    return 0;
+    // return 0;
   });
 
-  for (let [index, val] of array.entries()) {
-    arr.push(index + 1 + ". " + val);
-  }
-
-  return arr;
+  return array;
 };
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
@@ -35,14 +36,10 @@ const sortDescending = (array) => {
   array.sort((a, b) => {
     if (a.toLowerCase() < b.toLowerCase()) return 1;
     if (a.toLowerCase() > b.toLowerCase()) return -1;
-    return 0;
+    // return 0;
   });
 
-  for (let [index, val] of array.entries()) {
-    arr.push(index + 1 + ". " + val);
-  }
-
-  return arr;
+  return array;
 };
 
 // ! JANGAN DIMODIFIKASI
